d7491a10f9c74b2021337ecb1563c882af4e82be
GDennis01 <dennis.gobbi001@gmail.com>
2024-09-15 22:38:39 +0200

    feat: added switching windows mechanism and todo

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ def main():
                 [sg.Input(key='-IN-')],
                 [sg.Text("Max", key='-OUT2-')],
                 [sg.Input(key='-IN2-')],
-                [sg.Button('Go'), sg.Button('Exit')]]
+                [sg.Button('Go'),sg.Button('Custom list') ,sg.Button('Exit')]]
+    custom_list_layout_column = [[sg.Text('Enter file name')],
+                                [sg.Input(key='-FILE-')],
+                                [sg.Button('Go'), sg.Button('Standard list'),sg.Button('Exit')]]
+    
 
     show_kanji_layout_column = [[sg.Text('Kanji:',key='-KANJI-',font=('Helvetica', FONT_SIZE))],
                                 [sg.Text('Meaning:',key='-MEANING-',font=('Helvetica', FONT_SIZE),visible=True)],
@@ -18,7 +22,7 @@ def main():
                                 [sg.Button('Next')]]
 
     starting_layout = [[sg.VPush()],
-              [sg.Push(), sg.Column(starting_layout_column,element_justification='c'), sg.Push()],
+              [sg.Push(), sg.Column(starting_layout_column,element_justification='c',key='-L1-'),sg.Column(custom_list_layout_column,visible=False,key='-L2-'),sg.Push()],
               [sg.VPush()]]
     show_kanji_layout  = [
         [sg.VPush()],
@@ -27,25 +31,37 @@ def main():
     ]
 
     window = sg.Window('Window Title', starting_layout, size=(500,300))
-
+    layout=1
     while True:
         event, values = window.read()
         if event == sg.WIN_CLOSED or event == 'Exit':
             break
-        
-        min = values['-IN-']
-        max = values['-IN2-']
-        while not min.isdigit() or not max.isdigit() or int(min) > int(max):
-            sg.popup('Please enter a valid number range:')
-            _,values = window.read()
+        if event == 'Custom list':
+            window['-L1-'].update(visible=False)
+            window['-L2-'].update(visible=True)
+            layout = 2
+        if event == 'Standard list':
+            window['-L1-'].update(visible=True)
+            window['-L2-'].update(visible=False)
+            layout = 1
+        if layout == 1 and event == 'Go':
             min = values['-IN-']
             max = values['-IN2-']
-        min = int(min)
-        max = int(max)
-        print(min,max)
-        review_list = shuffle_kanji_list(kanji_list,min,max)
-        window.close()
-        break
+            while not min.isdigit() or not max.isdigit() or int(min) > int(max):
+                sg.popup('Please enter a valid number range:')
+                _,values = window.read()
+                min = values['-IN-']
+                max = values['-IN2-']
+            min = int(min)
+            max = int(max)
+            print(min,max)
+            review_list = shuffle_kanji_list(kanji_list,min,max)
+            window.close()
+            break
+        if layout == 2 and event == 'Go':
+            # TODO: leggere il file, salvarsi i numeri e creare la review list con solo i numeri salvati
+            pass
+
     window = sg.Window('Window Title', show_kanji_layout, size=(500,300),finalize=True)
     window['-KANJI-'].update(f'Kanji:{review_list[0].kanji} ')
     window['-MEANING-'].update(visible=False)
